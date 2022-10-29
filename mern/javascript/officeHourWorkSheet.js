54c4b3bd55e814900fd5361724b1f29f2dbee359
Monte DuMond <gdumond42@gmail.com>
2022-10-28 20:16:06 -0400

    added useState to Prop it Up

--- mern/javascript/officeHourWorkSheet.js
+++ mern/javascript/officeHourWorkSheet.js
@@ -1,21 +1,52 @@
-let nums = [2,7,8,1,4,6]
-let target = 9
+const nums1 = [2,7,11,15]
+const target1 = 9
+const nums2 = [3,2,4]
+const target2 = 6
+const nums3 = [3,3]
+const target3 = 6
+const nums4 = [3,2,3]
+const target4 = 6
 
+// BRUTE FORCE
+// const twoSum = (arr, trgt) => {
+//     for (let i=0; i<arr.length-1; i++){
+//         for (let j=i+1; j<arr.length-1; j++){
+//             if (arr[i]+arr[j]==trgt){
+//                 return [arr[i],arr[j]]
+//             }
+//         }
+//     }
+// }
+
+
+// My Solution
 const twoSum = (arr, trgt) => {
-    let answer = [];
-    for (let i=0; i<arr.length-1; i++){
-        for (let j=0; j<arr.length-1; j++){
-            if(i!=j){
-                if (arr[i]+arr[j]==trgt){
-                    answer.push([arr[i],arr[j]])
-                    const index = arr.indexOf(i)
-                    arr.splice(index,1)
-                }
-            }
+    for (let i=0; i<arr.length; i++) {
+        const remainder = trgt - arr[i]
+        if (arr.includes(remainder, i+1)){
+            arr.splice(i,1)
+            return [i, arr.indexOf(remainder)+1]
         }
     }
-    return answer;
 }
 
-console.log(twoSum(nums,target));
+// Class Solution
+// const twoSum = (nums, target) => {
+//     const map = {}
+    // for (let index = 0; index < nums.length; index++) {
+    //     const element = nums[index]
+    //     const complement = target - element
+    //     if (complement in map) {
+    //         return [map[complement], index]
+    //     } else {
+    //         map[element] = index
+    //     }
+    // }
+    // return []
+// }
+
+console.log(twoSum(nums1,target1));
+console.log(twoSum(nums2,target2));
+console.log(twoSum(nums3,target3));
+console.log(twoSum(nums4,target4));
 
--- mern/react/props/prop-it-up/src/components/PersonCard.jsx
+++ mern/react/props/prop-it-up/src/components/PersonCard.jsx
@@ -1,10 +1,21 @@
+import {useState} from 'react'
+
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor} = props
+    const [personAge, setPersonAge] = useState(age)
+
+    const handleBirthdayClick = () => {
+        setPersonAge(personAge+1)
+    }
+
     return (
         <div style={{border:"1px solid black",padding:"10px",width:"300px",margin:"10px auto"}}>
             <h2 style={{color:"red"}}>{lastName}, {firstName}</h2>
-            <h3>Age: {age}</h3>
+            <h3>Age: {personAge}</h3>
             <h3>Hair Color: {hairColor}</h3>
+            <button onClick={handleBirthdayClick}>
+                Birthday Button {firstName} {lastName}
+            </button>
         </div>
     )
 }
